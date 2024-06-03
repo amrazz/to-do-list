4884882ec29d23d095f1f3b00f62592c8e8767f7
Amraz Rafeeque <amrazrafeek2020@gmail.com>
2024-06-03 23:16:44 +0530

    initial commit

---
+++ todolist/myapp/static/js/index.js
@@ -0,0 +1,8 @@
+let Name = "Amraz Rafeek";
+let age = "19";
+let city = "Koduvally";
+
+
+document.getElementById("p1").textContent = Name;
+document.getElementById("p2").textContent = age;
+document.getElementById("p3").textContent = city;
